036b45ec132239d7ad94b1ff7427e9faf5764002
Aakash Verma <dev.aakashv@gmail.com>
2022-12-28 18:01:08 +0530

    Add the `Hero` section in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import HeroSection from "./components/HeroSection";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <HeroSection />
   );
 };
 
