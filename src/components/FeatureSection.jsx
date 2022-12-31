c1748d8c0dc326ba59013fa44340f306ceb243f6
Aakash Verma <dev.aakashv@gmail.com>
2022-12-31 19:01:29 +0530

    Render the `context` data

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -5,10 +5,22 @@ import SubscriptionButton from "./SubscriptionButton";
 
 const FeatureSection = () => {
     const { state } = useContext(Store);
-    console.log(state);
     return (
         <section>
             <ul>
+                { state.map((each, index) => (
+                    <li key={index}>
+                        <article>
+                            <figure>
+                                <img src={each.image} />
+                            </figure>
+                            <div>
+                                <h2>{ each.title }</h2>
+                                <p>{ each.description }</p>
+                            </div>
+                        </article>
+                    </li>
+                )) }
                 <li>
                     <h2>Ready To Build Your Community?</h2>
                     <SubscriptionButton>Get Started For Free</SubscriptionButton>
