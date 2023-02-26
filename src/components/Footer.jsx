73d5b7ce47d66763c973f534bbc1aeab199dae67
Aakash Verma <dev.aakashv@gmail.com>
2023-02-26 19:32:43 +0530

    Finish the `address` container

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,24 @@ import Logo from './Logo';
 const Footer = () => {
   return (
     <footer>
-        <Logo />
-        <img src={EmailIcon} />
-        <img src={LocationIcon} />
-        <img src={PhoneIcon} />
+        <div>
+            <Logo />
+            <address>
+                <div>
+                    <img src={LocationIcon} />
+                    <p>Aakash Verma, Punjab, INDIA 🇮🇳</p>
+                </div>
+                <div>
+                    <img src={PhoneIcon} />
+                    <a href="tel:+917009243305">+91-700-924-3305</a>
+                </div>
+                <div>
+                    <img src={EmailIcon} />
+                    <a href="mailto:dev.aakashv@gmail.com">dev.aakashv@gmail.com</a>
+                </div>
+            </address>
+        </div>
+        
     </footer>
   )
 };
