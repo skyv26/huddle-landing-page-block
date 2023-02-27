55af49be3c1839ddaff202de740e6782994e714a
Aakash Verma <dev.aakashv@gmail.com>
2023-02-27 20:59:49 +0530

    Create `Header` component for navigation

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Logo from "./Logo";
+
+const Header = () => {
+  return (
+    <header>
+        <a href="#">
+            <Logo />
+        </a>
+        <a href="">Try It Free</a>
+    </header>
+  )
+}
+
+export default Header;
