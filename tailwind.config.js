ef6b3806f2e25ddc05e5c9fdb6393225b128ecd8
Aakash Verma <dev.aakashv@gmail.com>
2023-02-28 21:16:56 +0530

    Add the font globally use by tailwind property

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // add the css variable and include fallback fonts from tailwind default theme
+        open_sans: ['Open Sans', ...fontFamily.serif],
+        poppins: ['Poppins', ...fontFamily.serif]
+      },
+    },
   },
   plugins: [],
 }
