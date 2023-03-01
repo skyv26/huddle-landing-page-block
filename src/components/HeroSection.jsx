e2f9b7331d9d06726cab25c330a6d81bc00192ab
Aakash Verma <dev.aakashv@gmail.com>
2023-03-01 21:43:03 +0530

    Add the `spacing`, `padding` to the hero-section content

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,14 +3,14 @@ import IllustrationMockUp from '../assets/illustration-mockups.svg';
 
 const HeroSection = () => {
   return (
-    <section>
-        <div className='flex flex-col'>
-            <h1 className='font-semibold text-[#00252E] text-lg'>Build The Community Your Fans Will Love</h1>
+    <section className='px-8 flex flex-col gap-14'>
+        <div className='flex flex-col pt-32 items-center text-center gap-4'>
+            <h1 className='font-semibold text-[#00252E] text-xl'>Build The Community Your Fans Will Love</h1>
             <p className='font-normal text-[#00252E] text-sm'>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
   Create connections with your users as you engage in genuine discussion. </p>
-            <SubscriptionButton className="text-white bg-[#FF52C1] text-xs">Get Started For Free</SubscriptionButton>
+            <SubscriptionButton className="text-white bg-[#FF52C1] text-xs max-w-max px-14 py-2.5 rounded-3xl">Get Started For Free</SubscriptionButton>
         </div>
-        <figure>
+        <figure className='pb-9'>
             <img src={IllustrationMockUp} />
         </figure>
     </section>
