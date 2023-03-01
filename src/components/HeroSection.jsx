b06f83c16820a04c4c3ddfedbf688ed3200f7da4
Aakash Verma <dev.aakashv@gmail.com>
2023-03-01 21:44:20 +0530

    Add the `background-color` to the `Hero` section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import IllustrationMockUp from '../assets/illustration-mockups.svg';
 
 const HeroSection = () => {
   return (
-    <section className='px-8 flex flex-col gap-14'>
+    <section className='px-8 flex flex-col gap-14 bg-[#EDFBFF]'>
         <div className='flex flex-col pt-32 items-center text-center gap-4'>
             <h1 className='font-semibold text-[#00252E] text-xl'>Build The Community Your Fans Will Love</h1>
             <p className='font-normal text-[#00252E] text-sm'>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
