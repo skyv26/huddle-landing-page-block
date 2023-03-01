ae2e7fb5f86899ebd68a794e4811167bd625bb66
Aakash Verma <dev.aakashv@gmail.com>
2023-03-01 21:31:31 +0530

    Add the `text` size, font-family and background color

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,11 +4,11 @@ import IllustrationMockUp from '../assets/illustration-mockups.svg';
 const HeroSection = () => {
   return (
     <section>
-        <div>
-            <h1>Build The Community Your Fans Will Love</h1>
-            <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
+        <div className='flex flex-col'>
+            <h1 className='font-semibold text-[#00252E] text-lg'>Build The Community Your Fans Will Love</h1>
+            <p className='font-normal text-[#00252E] text-sm'>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
   Create connections with your users as you engage in genuine discussion. </p>
-            <SubscriptionButton>Get Started For Free</SubscriptionButton>
+            <SubscriptionButton className="text-white bg-[#FF52C1] text-xs">Get Started For Free</SubscriptionButton>
         </div>
         <figure>
             <img src={IllustrationMockUp} />
