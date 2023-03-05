5f1d41c54ed3ee04aebb8f857a4b9079fbb95328
Aakash Verma <dev.aakashv@gmail.com>
2023-03-05 12:08:09 +0530

    Add the `styles` to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <header>
+    <header className="flex flex-row justify-between items-center absolute px-4 pt-7">
         <a href="#">
-            <Logo />
+            <Logo width="104" height="16" />
         </a>
-        <a href="">Try It Free</a>
+        <a href="" className="py-1 px-6 font-open-sans font-bold text-[10px] bg-white shadow-[0px_2px_7px_0px_rgba(0,0,0,0.1)] rounded-full">Try It Free</a>
     </header>
   )
 }
