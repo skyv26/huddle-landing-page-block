30baf995b4cf2cdd0c1cb34ca432b861521bd3e9
Aakash Verma <dev.aakashv@gmail.com>
2023-03-06 12:32:28 +0530

    Format the `document` for better visibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,17 @@ import Logo from "./Logo";
 const Header = () => {
   return (
     <header className="flex flex-row justify-between items-center absolute w-full px-4 pt-7">
-        <a href="#">
-            <Logo width="104" height="16" />
-        </a>
-        <a href="" className="py-1 px-6 font-open-sans font-bold text-[10px] bg-white shadow-[0px_2px_7px_0px_rgba(0,0,0,0.1)] rounded-full">Try It Free</a>
+      <a href="#">
+        <Logo width="104" height="16" />
+      </a>
+      <a
+        href=""
+        className="py-1 px-6 font-open-sans font-bold text-[10px] bg-white shadow-[0px_2px_7px_0px_rgba(0,0,0,0.1)] rounded-full"
+      >
+        Try It Free
+      </a>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
