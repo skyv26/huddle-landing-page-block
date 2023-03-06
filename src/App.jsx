5d3a6c14702b9717492a7dbce22a5fc18e0e95f1
Aakash Verma <dev.aakashv@gmail.com>
2023-03-06 12:33:20 +0530

    Format the `document` for better visibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
-import Footer from './components/Footer';
-import Header from './components/Header';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
