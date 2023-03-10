666186d0d506e930cc7ca698502b285454f127ab
Aakash Verma <dev.aakashv@gmail.com>
2023-03-10 12:34:26 +0530

    Format the `document` for better visibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import ContextProvider from './store/ContextProvider'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import ContextProvider from "./store/ContextProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ContextProvider>
       <App />
     </ContextProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
