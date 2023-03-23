280c900db8761959f7b6f720b6c235a2c087eafe
Aakash Verma <dev.aakashv@gmail.com>
2023-03-23 15:58:00 +0530

    Add the styles for `medium` phone screen in footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#00252E] px-4 pt-[139px] flex flex-col gap-10">
+    <footer className="bg-[#00252E] px-4 pt-[139px] flex flex-col gap-10 min-[390px]:pt-44 min-[390px]:px-6">
       <div className="flex flex-col gap-7">
         <figure className="-ml-1">
           <Logo className="text-white inline-block" width="167" height="26" />
         </figure>
-        <address className="flex flex-col gap-6 not-italic items-start font-open-sans text-sm text-white">
+        <address className="flex flex-col gap-6 not-italic items-start font-open-sans text-sm text-white min-[390px]:text-base">
           <div className="flex flex-row gap-4">
             <figure className="w-[24px] pt-[0.2rem]">
               <img src={LocationIcon} className="" />
@@ -32,7 +32,7 @@ const Footer = () => {
           </div>
         </address>
       </div>
-      <ul className="flex flex-col gap-4 font-open-sans text-sm text-white">
+      <ul className="flex flex-col gap-4 font-open-sans text-sm text-white min-[390px]:text-base">
         <li>
           <ul className="flex flex-col gap-4">
             <li className="hover:underline max-w-max">
@@ -62,19 +62,19 @@ const Footer = () => {
       </ul>
       <div className="flex flex-col gap-6 text-white items-center">
         <ul className="flex flex-row gap-3 items-center justify-center">
-          <li className="flex flex-row items-center justify-center w-8 h-8 border-[1px] border-white rounded-full hover:text-[#FF52C1] hover:border-[#FF52C1] hover:cursor-pointer">
+          <li className="flex flex-row items-center justify-center w-10 h-10 border-[1px] border-white rounded-full hover:text-[#FF52C1] hover:border-[#FF52C1] hover:cursor-pointer">
             <a href="https://www.facebook.com/skybrel">
-              <FaFacebookF />
+              <FaFacebookF className="text-xl" />
             </a>
           </li>
-          <li className="flex flex-row items-center justify-center w-8 h-8 border-[1px] border-white rounded-full hover:text-[#FF52C1] hover:border-[#FF52C1] hover:cursor-pointer">
+          <li className="flex flex-row items-center justify-center w-10 h-10 border-[1px] border-white rounded-full hover:text-[#FF52C1] hover:border-[#FF52C1] hover:cursor-pointer">
             <a href="https://twitter.com/devaakashv">
-              <FaTwitter />
+              <FaTwitter className="text-xl" />
             </a>
           </li>
-          <li className="flex flex-row items-center justify-center w-8 h-8 border-[1px] border-white rounded-full hover:text-[#FF52C1] hover:border-[#FF52C1] hover:cursor-pointer">
+          <li className="flex flex-row items-center justify-center w-10 h-10 border-[1px] border-white rounded-full hover:text-[#FF52C1] hover:border-[#FF52C1] hover:cursor-pointer">
             <a href="https://instagram.com/skybrel">
-              <FaInstagram />
+              <FaInstagram className="text-xl" />
             </a>
           </li>
         </ul>
