4a2e5a412bb41ff569ea01ec56e333f6400bfcef
Aakash Verma <dev.aakashv@gmail.com>
2023-03-23 15:38:42 +0530

    Increase the font size and spacing

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,17 +3,17 @@ import IllustrationMockUp from "../assets/illustration-mockups.svg";
 
 const HeroSection = () => {
   return (
-    <section className="px-4 flex flex-col gap-14 bg-[#EDFBFF] hero-section bg-no-repeat bg-contain bg-[top_right_0.1rem] items-center">
-      <div className="flex flex-col pt-32 items-center text-center gap-4">
-        <h1 className="px-9 font-semibold text-[#00252E] text-xl">
+    <section className="px-4 flex flex-col gap-14 bg-[#EDFBFF] hero-section bg-no-repeat bg-contain bg-[top_right_0.1rem] items-center min-[390px]:h-screen min-[390px]:px-6 min-[390px]:gap-16">
+      <div className="flex flex-col pt-32 items-center text-center gap-4 min-[390px]:gap-8">
+        <h1 className="px-9 font-semibold text-[#00252E] text-xl min-[390px]:text-3xl">
           Build The Community Your Fans Will Love
         </h1>
-        <p className="px-3 font-normal font-open-sans text-[#00252E] text-sm">
+        <p className="px-3 font-normal font-open-sans text-[#00252E] text-sm min-[390px]:text-base">
           Huddle re-imagines the way we build communities. You have a voice, but
           so does your audience. Create connections with your users as you
           engage in genuine discussion.{" "}
         </p>
-        <SubscriptionButton className="font-open-sans font-bold text-white bg-[#FF52C1] text-xs max-w-max px-14 py-2.5 rounded-3xl shadow-[2px_3px_6px_1px_rgba(255,82,193,0.17)] hover:bg-[#FF9ADB] hover:cursor-pointer">
+        <SubscriptionButton className="font-open-sans font-bold text-white bg-[#FF52C1] text-xs max-w-max px-14 py-2.5 rounded-3xl shadow-[2px_3px_6px_1px_rgba(255,82,193,0.17)] hover:bg-[#FF9ADB] hover:cursor-pointer min-[390px]:text-sm min-[390px]:px-20 min-[390px]:py-4">
           Get Started For Free
         </SubscriptionButton>
       </div>
