4b8b3721e6e1b18e0bd56031eab108cee91a6b20
Aakash Verma <dev.aakashv@gmail.com>
2023-03-25 17:23:48 +0530

    Add the `tablet` responsive spacing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <header className="flex flex-row justify-between items-center absolute w-full px-4 pt-7 min-[390px]:px-6">
+    <header className="flex flex-row justify-between items-center absolute w-full px-4 pt-7 min-[390px]:px-6 md:px-8 md:pt-10">
       <a href="#">
-        <Logo width="104" height="16" />
+        <Logo className="w-[104px] h-[16px] md:w-[200px] md:h-[31px]" />
       </a>
       <a
         href="https://devaakash.com/"
         target="_blank"
         rel="noreferrer noopener"
-        className="py-1 px-6 font-open-sans font-bold text-[10px] bg-white shadow-[0px_2px_7px_0px_rgba(0,0,0,0.1)] rounded-full"
+        className="py-1 px-6 font-open-sans font-bold text-[10px] bg-white shadow-[0px_2px_7px_0px_rgba(0,0,0,0.1)] rounded-full md:text-sm md:px-[4.0325rem] md:py-[0.875rem]"
       >
         Try It Free
       </a>
