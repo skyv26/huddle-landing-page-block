1b4e46a9f63ff42f72b6c8d0dbbbbf50709151de
Aakash Verma <dev.aakashv@gmail.com>
2023-03-26 13:31:53 +0530

    Update the `footer` by adding the bottom spacing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -78,7 +78,7 @@ const Footer = () => {
             </a>
           </li>
         </ul>
-        <div className="flex flex-col gap-1 text-xs text-center">
+        <div className="flex flex-col gap-1 text-xs text-center pb-8">
           <p>
             &copy; Copyright {new Date().getFullYear()} Huddle. All rights
             reserved.
