8e9563a6d21f13c0881f4ad9e21eb80932a8f8bb
Aakash Verma <dev.aakashv@gmail.com>
2023-04-05 22:05:32 +0530

    Increase the `text` size for the `large` screen

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -25,21 +25,21 @@ const FeatureSection = () => {
                 <img src={each.image} />
               </figure>
               <div className="flex flex-col gap-4 text-center md:text-left">
-                <h2 className="text-[#00252E] text-[20px] font-poppins font-semibold min-[390px]:text-2xl md:text-xl">
+                <h2 className="text-[#00252E] text-[20px] font-poppins font-semibold min-[390px]:text-2xl md:text-xl lg:text-2xl">
                   {each.title}
                 </h2>
-                <p className="text-[#808E9A] text-[14px] font-open-sans font-normal min-[390px]:text-base md:text-sm">
+                <p className="text-[#808E9A] text-[14px] font-open-sans font-normal min-[390px]:text-base md:text-sm lg:text-base">
                   {each.description}
                 </p>
               </div>
             </article>
           </li>
         ))}
-        <li className="flex flex-col items-center gap-[24px] text-center px-3 py-10 bg-white shadow-[0_0_14px_0px_rgba(0,0,0,0.07)] rounded-2xl w-full translate-y-[70%] min-[390px]:px-4 min-[390px]:py-12 md:max-w-max md:px-20 md:z-10">
-          <h2 className="font-poppins font-semibold text-[16px] text-[#00252E] min-[390px]:text-xl md:text-lg">
+        <li className="flex flex-col items-center gap-[24px] text-center px-3 py-10 bg-white shadow-[0_0_14px_0px_rgba(0,0,0,0.07)] rounded-2xl w-full translate-y-[70%] min-[390px]:px-4 min-[390px]:py-12 md:max-w-max md:px-20 md:z-10 lg:gap-10">
+          <h2 className="font-poppins font-semibold text-[16px] text-[#00252E] min-[390px]:text-xl md:text-lg lg:text-[30px]">
             Ready To Build Your Community?
           </h2>
-          <SubscriptionButton className="text-white bg-[#FF52C1] font-open-sans font-bold text-xs max-w-max px-14 py-2.5 rounded-3xl hover:bg-[#FF9ADB] hover:cursor-pointer min-[390px]:text-md">
+          <SubscriptionButton className="text-white bg-[#FF52C1] font-open-sans font-bold text-xs max-w-max px-14 py-2.5 rounded-full hover:bg-[#FF9ADB] hover:cursor-pointer min-[390px]:text-sm lg:text-base lg:px-16 lg:py-4">
             Get Started For Free
           </SubscriptionButton>
         </li>
