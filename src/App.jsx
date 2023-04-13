1be60d41ebaccf5d73c61cf8f18ef93804487ca4
Aakash Verma <dev.aakashv@gmail.com>
2023-04-14 03:56:13 +0530

    Add the `darkMode` toggleable button with context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Store from "./store/store";
+import { SWITCH_THEME } from "./store/ContextProvider";
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+
 
 const App = () => {
+  const { state, dispatch } = useContext(Store);
+
+  const toggleDarkMode = () => {
+    const { data, mode } = state;
+    const newObj = {
+      data,
+      mode: !mode,
+    };
+    dispatch({ type: SWITCH_THEME, payload: newObj });
+  };
+
   return (
     <React.Fragment>
       <Header />
@@ -13,6 +28,12 @@ const App = () => {
         <FeatureSection />
       </main>
       <Footer />
+      <DarkModeSwitch
+      style={{ marginBottom: '2rem' }}
+      checked={state.mode}
+      onChange={toggleDarkMode}
+      size={120}
+    />
     </React.Fragment>
   );
 };
