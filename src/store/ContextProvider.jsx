6656c673d7364af485a37cc52948115bfe11ee36
Aakash Verma <dev.aakashv@gmail.com>
2023-04-15 05:26:52 +0530

    Add the `alt` property

--- src/store/ContextProvider.jsx
+++ src/store/ContextProvider.jsx
@@ -15,18 +15,22 @@ const initialState = {
       description:
         "Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form.",
       image: GrowTogether,
+      alt: 'Let\'s grow together' 
     },
     {
       title: "Flowing Conversations",
       description:
         "You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.",
       image: FlowingTogether,
+      alt: 'Let\'s flow together' 
     },
     {
       title: "Your Users",
       description:
         "It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.",
       image: YourUser,
+      alt: 'Know your user' 
+
     },
   ],
   mode: false,
