79eef8c1f5df3a67472a8c9558ebf487b438d179
Aakash Verma <dev.aakashv@gmail.com>
2023-04-22 07:25:43 +0530

    Write test for `Header` component

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Header from '../components/Header';
+
+test('renders learn react link', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
