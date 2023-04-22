b258dde09dc66a300fd2c74d3485b5f68ab0341b
Aakash Verma <dev.aakashv@gmail.com>
2023-04-22 08:23:32 +0530

    Update the `Header.test.js` file

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,8 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react';
 import Header from '../components/Header';
+import ContextProvider from '../store/ContextProvider';
 
 test('renders learn react link', () => {
-  render(<Header />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <ContextProvider>
+        <Header />
+    </ContextProvider>
+  );
+  const linkElement = screen.getByText(/Try it free/i);
   expect(linkElement).toBeInTheDocument();
 });
