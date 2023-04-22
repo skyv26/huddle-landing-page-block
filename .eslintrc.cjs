4c7c3a783e0e165e2573254dedde4f0faadda0ef
Aakash Verma <dev.aakashv@gmail.com>
2023-04-22 08:22:24 +0530

    Add the configuration and enable the jsdom environment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,11 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    "react-app", 
-    "react-app/jest",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
