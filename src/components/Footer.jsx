d94eeb74e88c4a9a04816b711f49adc8d881adea
Aakash Verma <dev.aakashv@gmail.com>
2023-09-25 20:54:35 +0530

    Complete the `Footer` part skeleton

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import EmailIcon from '../assets/icon-email.svg';
 import LocationIcon from '../assets/icon-location.svg';
 import PhoneIcon from '../assets/icon-phone.svg';
 import Logo from './Logo';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
   return (
@@ -23,7 +24,58 @@ const Footer = () => {
                 </div>
             </address>
         </div>
-        
+        <ul>
+            <li>
+                <ul>
+                    <li>
+                        <a href="#">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#">What We Do</a>
+                    </li>
+                    <li>
+                        <a href="#">FAQ</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <ul>
+                    <li>
+                        <a href="#">Career</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <div>
+            <ul>
+                <li>
+                    <a href="">
+                        <FontAwesomeIcon icon="fa-brands fa-facebook-f" />
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <FontAwesomeIcon icon="fa-brands fa-twitter" />
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <FontAwesomeIcon icon="fa-brands fa-instagram" />
+                    </a>
+                </li>
+            </ul>
+            <p>&copy; Copyright {new Date().getFullYear()} Huddle. All rights reserved.</p>
+        </div>
+        <p>
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer noopener" >Frontend Mentor</a>. 
+            Coded by <a href="#">Aakash Verma ❤️</a>.
+        </p>
     </footer>
   )
 };
